bfd333874d5281c276a61c286e1386c0fd070956
gonelastvirus <gonelastvirus@gmail.com>
2025-08-24 23:57:57 +0545

    build add for render

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Rust in temporary location
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -q
+sleep 2
+
+# Set Cargo/Rust paths
+export CARGO_HOME=/tmp/.cargo
+export RUSTUP_HOME=/tmp/.rustup
+export PATH=$CARGO_HOME/bin:$PATH
+sleep 2
+
+# Upgrade pip and install dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
